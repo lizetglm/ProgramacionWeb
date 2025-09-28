dc49ee0802bf8331a397084047cb465f9ac65168
lizetglm <lizetglm@gmail.com>
2025-09-28 15:02:54 -0600

    Fomulrios

--- formulario/archivos/forms.py
+++ formulario/archivos/forms.py
@@ -1,12 +1,33 @@
 from django import forms
+from .models import Estudiante, Archivos
 
 
 #permite subir multiples archivos
-# class MultipleFileInput(forms.ClearableFileInput):
-#     allow_multiple_selected = True
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
 
-# class ArchivosForm(forms.Form):
-#     nombre = forms.CharField(label="Nombre", max_length=100)
-#     carrera = forms.CharField(label="Carrera", max_length=100)
-#     semestre = forms.IntegerField(label="Semestre")
-#     archivo = forms.FileField(label="Archivo")
+
+#permite recibir multiples archivos
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput(attrs={"multiple": True}))
+        super().__init__(*args, **kwargs)
+
+#validar los datos del formulario
+    def clean(self, data, initial=None):
+        files = super().clean(data, initial)
+        if not isinstance(data, (list, tuple)):
+            data = [data]
+        for file in data:
+            if not file.content_type in ["application/pdf", "image/jpeg", "image/png"]:
+                raise forms.ValidationError("Solo se permiten archivos PDF o imágenes (JPG, PNG).")
+        return data
+
+
+#formulario para el modelo Estudiante
+class EstudianteForm(forms.ModelForm):
+    archivos = MultipleFileField(required=False)
+    
+    class Meta:
+        model = Estudiante
+        fields = ["nombre", "carrera", "semestre", "archivos"]
